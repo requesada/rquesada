83f6c17ff5b6c08b4fa742666d22c5756287f941
Roberto Quesada <rquesada@protonmail.ch>
2024-05-24 15:29:16 -0700

    Make necessary changes to get images working with webpack and to get rotating grid working

--- src/App.js
+++ src/App.js
@@ -1,53 +1,36 @@
-import React, { useRef, useEffect } from "react"
-import gsap from "gsap"
+import React from "react"
 import "./App.css"
 
+// flower images
+import flower1 from './images/flower1.jpg'
+import flower2 from './images/flower2.jpg'
+import flower3 from './images/flower3.jpg'
+import flower4 from './images/flower4.jpg'
+import flower5 from './images/flower5.jpg'
+import flower6 from './images/flower6.jpg'
+import flower7 from './images/flower7.jpg'
+import flower8 from './images/flower8.jpg'
+import flower9 from './images/flower9.jpg'
+const flowers = [flower1, flower2, flower3, flower4, flower5, flower6, flower7, flower8, flower9]
+
 const App = () => {
-    const gridItemsRef = useRef([])
-
-    useEffect(() => {
-        gridItemsRef.current.forEach((item) => {
-            gsap.to(item, {
-                rotationY: 180,
-                duration: 1,
-                paused: true,
-                reversed: true,
-            })
-        })
-    }, [])
-
-    const handleMouseEnter = (index) => {
-        gsap.to(gridItemsRef.current[index], {
-            rotationY: 180,
-            duration: 1,
-        })
-    }
-
-    const handleMouseLeave = (index) => {
-        gsap.to(gridItemsRef.current[index], {
-            rotationY: 0,
-            duration: 1,
-        })
-    }
-
-    return (
-        <div className="App">
-            <h1>Hi</h1>
-            <div className="image-grid-container">
-                {Array.from({length: 9}).map((_, index) => (
-                    <div
-                        className="image-grid-item"
-                        key={index}
-                        ref={(element) => (gridItemsRef.current[index] = element)}
-                        onMouseEnter={() => handleMouseEnter(index)}
-                        onMouseLeave={() => handleMouseLeave(index)}
-                    >
-                        {index + 1}
-                    </div>
-                ))}
+  return (
+    <div className="App">
+      <h1>Roberto Quesada</h1>
+      <div className="image-grid-container">
+        {flowers.map((src, index) => (
+          <div className="image-grid-item" key={index}>
+            <div className="image-grid-item-inner">
+              <div className="front-face"></div>
+              <div className="back-face">
+                <img src={src} alt={`flower ${index + 1}`} />
+              </div>
             </div>
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App
