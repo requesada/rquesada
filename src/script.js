05d5841b09b7ebc14335aebc091f4b14cfe74b0f
Roberto Quesada <rquesada@protonmail.ch>
2024-05-25 15:32:21 -0700

    Fooling around with GSAP

--- src/script.js
+++ src/script.js
@@ -1,4 +1,14 @@
 import './styles.css' // Have to import to bypass webpack MIME issue
+import {gsap} from 'gsap'
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#content').style.display = 'flex'
+  gsap.to('#content', {
+    scale: 1,
+    duration: 2,
+    ease: 'power2.out'
+  })
+})
 
 if (module.hot) {
   module.hot.accept();
