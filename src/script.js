3eabc793a0a201c09d46317ffdcb43d70fbb04b8
Roberto Quesada <rquesada@protonmail.ch>
2024-06-11 17:15:35 -0700

    Various structural and aesthetic tweaks

--- src/script.js
+++ src/script.js
@@ -3,17 +3,21 @@ import gsap from 'https://cdn.skypack.dev/gsap'
 
 const backFace = document.querySelector('#back')
 const leftFace = document.querySelector('#left')
+const rightFace = document.querySelector('#right')
 const topFace = document.querySelector('#top')
+const bottomFace = document.querySelector('#bottom')
 
 let width
 let height
 
 const cuboidDepth = {value: 6000}
 
+const lightColors = ['#002d7a', '#b8d6bc']
+
 const randomActivation = setInterval(() => {
   const index = randomIndex()
   gsap.to(`#cell-${index}`, {
-    backgroundColor: 'red',
+    backgroundColor: gsap.utils.random(lightColors),
     duration: randomDuration(),
     ease: 'elastic.out(1, 0.1)',
     onComplete: () => {
@@ -46,8 +50,8 @@ const changeDepth = () => {
 backFace.addEventListener('click', changeDepth)
 
 const getDimensions = () => {
-  width = window.innerWidth - 10
-  height = window.innerHeight - 10
+  width = window.innerWidth
+  height = window.innerHeight
 }
 
 const formatTransform = (axis, deg, xPx, yPx, zPx) => {
@@ -66,13 +70,21 @@ const setCuboidTransforms = () => {
   leftFace.style.transform = formatTransform('Y', -90, -cuboidDepth.value / 2, 0, cuboidDepth.value / 2)
   leftFace.style.width = `${cuboidDepth.value}px`
   leftFace.style.height = `${height}px`
+  
+  rightFace.style.transform = formatTransform('Y', 90, cuboidDepth.value / 2, 0, width - cuboidDepth.value / 2)
+  rightFace.style.width = `${cuboidDepth.value}px`
+  rightFace.style.height = `${height}px`
 
   topFace.style.transform = formatTransform('X', 90, 0, -cuboidDepth.value / 2, cuboidDepth.value / 2)
   topFace.style.width = `${width}px`
   topFace.style.height = `${cuboidDepth.value}px`
+  
+  bottomFace.style.transform = formatTransform('X', -90, 0, cuboidDepth.value / 2, height - cuboidDepth.value / 2)
+  bottomFace.style.width = `${width}px`
+  bottomFace.style.height = `${cuboidDepth.value}px`
 }
 
-const gridSide = 5
+const gridSide = 20
 const root = document.querySelector(':root')
 root.style.setProperty('--grid-side', gridSide)
 
@@ -88,7 +100,7 @@ const findAdjacentCells = (index) => {
   return {adjacentCells: [top, bottom, left, right], isTriggered: false}
 }
 
-const randomIndex = gsap.utils.random(0, Math.pow(gridSide, 2), 1, true)
+const randomIndex = gsap.utils.random(0, Math.pow(gridSide, 2) - 1, 1, true)
 const randomDuration = gsap.utils.random(0.1, 2, 0.1, true)
 
 let cascadeStarted = false
@@ -104,7 +116,7 @@ const lightOff = (cellID) => {
 const lightOn = (cellID) => {
   if (depthChanged && !cascadeStarted) {
     gsap.to(`#${cellID}`, {
-      backgroundColor: 'red',
+      backgroundColor: gsap.utils.random(lightColors),
       duration: 0.5,
       ease: 'elastic.out(1, 0.1)',
       onInterrupt: () => lightOff(cellID)
@@ -119,7 +131,7 @@ const triggerCascade = (cellID) => {
       gsap.killTweensOf(`#${cellID}`)
       gsap.to(`#${cellID}`, {
         backgroundColor: 'transparent',
-        duration: 0.1,
+        duration: 0.01,
         onComplete: () => {
           cellLayout[cellID].adjacentCells.forEach((cell) => triggerCascade(cell))
           cellLayout[cellID].isTriggered = true
@@ -134,7 +146,7 @@ const gridContainer = document.querySelector('#grid-container')
 for (let i = 0; i < Math.pow(gridSide, 2); i++) {
   const gridCell = document.createElement('div')
   gridCell.className = 'grid-cell'
-  gridCell.textContent = i
+  // gridCell.textContent = i
   gridContainer.appendChild(gridCell)
   
   const cellID = `cell-${i}`
@@ -154,4 +166,5 @@ window.addEventListener('resize', onResize)
 document.addEventListener('DOMContentLoaded', () => {
   getDimensions()
   setCuboidTransforms()
+  document.querySelector('#cuboid').style.display = 'block'
 })
